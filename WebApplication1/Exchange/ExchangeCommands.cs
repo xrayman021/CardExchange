571cca9449ef27b16feced465e2e68fcd25b8c97
xrayman021 <82471086+xrayman021@users.noreply.github.com>
2026-01-20 11:55:45 -0600

    Created Exchange folder and ExchangeCommands script in api

---
+++ WebApplication1/Exchange/ExchangeCommands.cs
@@ -0,0 +1,11 @@
+﻿using CardExchange.core.Domain;
+
+namespace CardExchange.Api.Exchange;
+
+public interface IExchangeCommand { }
+
+public sealed record DepositCashCmd(Guid UserId, long Cents, TaskCompletionSource<object> Tcs) : IExchangeCommand;
+
+public sealed record DepositInvCmd(Guid UserId, SkuId Sku, int Qty, TaskCompletionSource<object> Tcs) : IExchangeCommand;
+
+public sealed record GetBalanceCmd(Guid UserId, TaskCompletionSource<object> Tcs) : IExchangeCommand;
