97a8117010cea7b2d7cd94acabcc5986fd52b13c
xrayman021 <82471086+xrayman021@users.noreply.github.com>
2026-01-20 17:32:15 -0600

    Created BookRef class

---
+++ CardExchange.core/OrderBooks/BookRef.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CardExchange.core.OrderBooks
+{
+    public sealed class BookRef
+    {
+        public long PriceCents { get; init; }
+        public required LinkedListNode<CardExchange.Core.Exchange.Order> Node { get; init; }
+    }
+}
